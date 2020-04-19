94892d22ff4f8fb2c9d27b7a3985af43e9e001ef
llleyelll <chida.fua@gmail.com>
2020-04-19 21:29:14 +0700

    Finished

---
+++ src/LegacyAdapter.java
@@ -0,0 +1,37 @@
+public class LegacyAdapter implements IUnit {
+    private LegacyUnit legacyUnit;
+
+    public LegacyAdapter(LegacyUnit legacyUnit) {
+        this.legacyUnit = legacyUnit;
+    }
+
+    @Override
+    public void move() {
+        legacyUnit.walk();
+    }
+
+    @Override
+    public int getX() {
+        return legacyUnit.getPositionX();
+    }
+
+    @Override
+    public int getY() {
+        return legacyUnit.getPositionY();
+    }
+
+    @Override
+    public String getName() {
+        return "LegacyUnit";
+    }
+
+    @Override
+    public int getHealth() {
+        return 100;
+    }
+
+    @Override
+    public boolean dead() {
+        return false;
+    }
+}
